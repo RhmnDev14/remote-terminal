6efd21dc9a261993c0d4990cc07caf0b52e195a6
Rahman Umardi <rahmanumardi@gmail.com>
2026-01-04 14:05:05 +0700

    change header

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title ?? 'Rahman Terminal' }}</title>
+        <title>{{ $title ?? 'Remote Terminal' }}</title>
         
         <!-- PWA Manifest & Meta -->
         <link rel="manifest" href="/manifest.json">
